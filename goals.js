cfbcf5b8410c1fdd8576de5dd1e5bc5e758fb010
krewdreele <keele.drew00@gmail.com>
2024-02-22 22:04:35 -0700

    goals javascript finished

--- goals.js
+++ goals.js
@@ -30,3 +30,25 @@ function editMainGoal(){
     goal_date_in.textContent = goal['goal-date'];
     goal_weight_in.textContent = goal['goal-weight'];
 }
+
+function saveDailyGoal(){
+    let ltgt_calorie = document.getElementById("ltgt-calorie");
+    let ltgt_protein = document.getElementById("ltgt-protein");
+    let ltgt_fat = document.getElementById("ltgt-fat");
+    let ltgt_carb = document.getElementById("ltgt-carb");
+
+    let calorie_in = document.getElementById("calorie-in");
+    let protein_in = document.getElementById("protein-in");
+    let fat_in = document.getElementById("fat-in");
+    let carb_in = document.getElementById("carb-in");
+
+    let calorie_goal = document.getElementById("calorie-daily-goal");
+    let protein_goal = document.getElementById("protein-daily-goal");
+    let fat_goal = document.getElementById("fat-daily-goal");
+    let carb_goal = document.getElementById("carb-daily-goal");
+
+    calorie_goal.textContent = `Calories: ${ltgt_calorie.value} ${calorie_in.value}`;
+    protein_goal.textContent = `Protein: ${ltgt_protein.value} ${protein_in.value}g`;
+    fat_goal.textContent = `Fat: ${ltgt_fat.value} ${fat_in.value}g`;
+    carb_goal.textContent = `Carbs: ${ltgt_carb.value} ${carb_in.value}g`;
+}
