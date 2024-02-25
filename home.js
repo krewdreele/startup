5a2374254106566d0704d29ee8c19114f5973df3
krewdreele <keele.drew00@gmail.com>
2024-02-24 20:46:27 -0700

    started on main page javascript

--- home.js
+++ home.js
@@ -1,2 +1,12 @@
 document.getElementById("username").textContent = localStorage.getItem("this_user");
 
+let total_calories = localStorage.getItem("total-cals") ?? 0;
+let total_protein = localStorage.getItem("total-protein") ?? 0;
+let total_fat = localStorage.getItem("total-fat") ?? 0;
+let total_carbs = localStorage.getItem("total-carbs") ?? 0;
+
+let date = new Date();
+
+function addFood(){
+
+}
