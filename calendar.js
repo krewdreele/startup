ebc8ffda6b31dd25216c33ccddab88d79bf91baf
krewdreele <keele.drew00@gmail.com>
2024-02-25 16:02:06 -0700

    started the log javascript

---
+++ calendar.js
@@ -0,0 +1,69 @@
+class Month {
+    constructor(name, start_day, num_days){
+        this.name = name;
+        this.start_day = start_day;
+        this.num_days = num_days;
+    }
+}
+
+/*
+    sunday - 0
+    monday - 1
+    tuesday - 2
+    wednesday - 3
+    thursday - 4
+    friday - 5
+    saturday - 6
+*/
+
+let months = [
+    new Month("January", 1, 31),
+    new Month("February", 4, 29),
+    new Month("March", 5, 31),
+    new Month("April", 1, 30)
+]
+
+let current_month = months[0];
+document.getElementById("month-name").textContent = current_month.name;
+setCalendar(months[0]);
+
+function setCalendar(month){
+    let calendar = document.getElementById("calendar");
+    calendar.textContent = '';
+    for(let i = 0; i < month.start_day; i++){
+        let blank = document.createElement("button");
+        blank.className = "btn btn-light";
+        calendar.appendChild(blank);
+    }
+
+    for(let i=1; i <= month.num_days; i++){
+        let date = current_month.name + " " + i;
+        
+        let log_info = localStorage.getItem(date) ?? "";
+        if(log_info){
+            log_info = JSON.parse(log_info);
+        }
+
+        let hit_goal = log_info?.hit_goal ?? 2;
+
+        let day = document.createElement("button");
+
+        if(hit_goal === 1){
+            day.className = "btn btn-outline-success";
+        }
+        else if(hit_goal === 0){
+            day.className = "btn btn-outline-danger";
+        }
+        else if(hit_goal === 2) {
+            day.className = "btn btn-light";
+        }
+
+        day.setAttribute("data-bs-toggle", "modal");
+        day.setAttribute("data-bs-target", "#calendar-date");
+        day.textContent = i;
+        calendar.appendChild(day);
+    }
+}
+
+
+
