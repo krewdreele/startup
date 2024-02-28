7f894c801866c2e06ea27ebeb3cfae78b9242b84
krewdreele <keele.drew00@gmail.com>
2024-02-27 21:41:05 -0700

    Most functionality finished, just need to clean it up a bit and add friends

--- meals.js
+++ meals.js
@@ -1,56 +1,102 @@
 let mode = "Breakfast";
 
-function changeMode(element){
-    mode = element.textContent.trim();
+function changeMode(element) {
+  mode = element.textContent.trim();
 }
 
-function loadMeals(){
-    document.getElementById("username").textContent = localStorage.getItem("this_user");
+function loadMeals() {
+  document.getElementById("username").textContent =
+    localStorage.getItem("this_user");
 
-    let breakfast_container = document.getElementById("Breakfast").children[0];
-    let lunch_container = document.getElementById("Lunch").children[0];
-    let dinner_container = document.getElementById("Dinner").children[0];
+  let breakfast_container = document.getElementById("Breakfast").children[0];
+  let lunch_container = document.getElementById("Lunch").children[0];
+  let dinner_container = document.getElementById("Dinner").children[0];
 
-    for (var i in localStorage) {
-
-        try{
-            item = JSON.parse(localStorage[i]);
-        }
-        catch(error){
-            continue;
-        }
-        
-        if(!item.hasOwnProperty("type")){
-            continue;
-        }
-        if(item.type == "Breakfast"){
-            createCard(item, breakfast_container);
-        }
-        else if(item.type == "Lunch"){
-            createCard(item, lunch_container);
-        }
-        else if(item.type == "Dinner"){
-            createCard(item, dinner_container);
-        }
+  for (var i in localStorage) {
+    try {
+      item = JSON.parse(localStorage[i]);
+    } catch (error) {
+      continue;
     }
+
+    if (!item.hasOwnProperty("type")) {
+      continue;
+    }
+    if (item.type == "Breakfast") {
+      createCard(item, breakfast_container);
+    } else if (item.type == "Lunch") {
+      createCard(item, lunch_container);
+    } else if (item.type == "Dinner") {
+      createCard(item, dinner_container);
+    }
+  }
 }
 
-function createCard(item, container){
+function createCard(item, container) {
+  let card = document.createElement("div");
+  card.className = "card";
+
+  let html_img = document.createElement("img");
+
+  let body = document.createElement("div");
+  body.className = "card-body";
+
+  let meal_name = document.createElement("h5");
+  meal_name.textContent = item.name;
+
+  let meal_desc = document.createElement("p");
+  meal_desc.className = "card-text";
+  meal_desc.textContent = item.description;
+
+  let button = document.createElement("button");
+  button.className = "btn btn-info";
+  button.setAttribute("data-bs-target", "#meal-info");
+  button.setAttribute("data-bs-toggle", "modal");
+  button.setAttribute("onclick", "getInfo(this)");
+  button.textContent = "Info";
+
+  card.appendChild(html_img);
+  card.appendChild(body);
+  body.appendChild(meal_name);
+  body.appendChild(meal_desc);
+  body.appendChild(button);
+
+  container.appendChild(card);
+}
+
+function saveMeal() {
+  let name = document.getElementById("name");
+  let desc = document.getElementById("desc");
+  let calories = document.getElementById("cals");
+  let protein = document.getElementById("protein");
+  let fat = document.getElementById("fat");
+  let carbs = document.getElementById("carbs");
+  let img = document.getElementById("meal-img");
+
+  if (
+    name.value &&
+    desc.value &&
+    calories.value &&
+    protein.value &&
+    fat.value &&
+    carbs.value
+  ) {
+    let card_container = document.getElementById(mode).children[0];
 
     let card = document.createElement("div");
     card.className = "card";
 
     let html_img = document.createElement("img");
-    
+
     let body = document.createElement("div");
     body.className = "card-body";
-    
+
     let meal_name = document.createElement("h5");
-    meal_name.textContent = item.name;
+    meal_name.textContent = name.value;
 
     let meal_desc = document.createElement("p");
     meal_desc.className = "card-text";
-    meal_desc.textContent = item.description;
+    meal_desc.textContent = desc.value;
 
     let button = document.createElement("button");
     button.className = "btn btn-info";
@@ -65,215 +111,183 @@ function createCard(item, container){
     body.appendChild(meal_desc);
     body.appendChild(button);
 
-    container.appendChild(card);
+    card_container.appendChild(card);
+
+    localStorage.setItem(
+      name.value,
+      JSON.stringify({
+        type: mode,
+        name: name.value,
+        description: desc.value,
+        calories: calories.value,
+        protein: protein.value,
+        fat: fat.value,
+        carbs: carbs.value,
+      })
+    );
+
+    name.value = "";
+    desc.value = "";
+    calories.value = "";
+    protein.value = "";
+    fat.value = "";
+    carbs.value = "";
+  } else {
+    alert();
+  }
 }
 
+function alert() {
+  meal_alert = document.getElementById("meal-alert");
+  meal_alert.style.display = "block";
+  meal_alert.textContent = "Please fill out all the fields!";
 
-function saveMeal(){
-    let name = document.getElementById("name");
-    let desc = document.getElementById("desc");
-    let calories = document.getElementById("cals");
-    let protein = document.getElementById("protein");
-    let fat = document.getElementById("fat");
-    let carbs = document.getElementById("carbs");
-    let img = document.getElementById("meal-img");
-
-    if(name.value && desc.value && calories.value && protein.value && fat.value && carbs.value){
-        let card_container = document.getElementById(mode).children[0];
-
-        let card = document.createElement("div");
-        card.className = "card";
-
-        let html_img = document.createElement("img");
-        
-        let body = document.createElement("div");
-        body.className = "card-body";
-        
-        let meal_name = document.createElement("h5");
-        meal_name.textContent = name.value;
-
-        let meal_desc = document.createElement("p");
-        meal_desc.className = "card-text";
-        meal_desc.textContent = desc.value;
-
-        let button = document.createElement("button");
-        button.className = "btn btn-info";
-        button.setAttribute("data-bs-target", "#meal-info");
-        button.setAttribute("data-bs-toggle", "modal");
-        button.setAttribute("onclick", "getInfo(this)");
-        button.textContent = "Info";
-
-        card.appendChild(html_img);
-        card.appendChild(body);
-        body.appendChild(meal_name);
-        body.appendChild(meal_desc);
-        body.appendChild(button);
-
-        card_container.appendChild(card);
-
-        localStorage.setItem(name.value, JSON.stringify({
-            "type" : mode,
-            "name" : name.value,
-            "description" : desc.value,
-            "calories" : calories.value,
-            "protein" : protein.value,
-            "fat" : fat.value,
-            "carbs" : carbs.value
-        }))
-
-        name.textContent = "";
-        desc.textContent = "";
-        calories.textContent = "";
-        protein.textContent = "";
-        fat.textContent = "";
-        carbs.textContent = "";
-    }
-    else{
-        alert();
-    }
+  setTimeout(() => {
+    meal_alert.style.display = "none";
+  }, 2500);
 }
 
-function alert(){
-    meal_alert = document.getElementById("meal-alert");
-    meal_alert.style.display = "block";
-    meal_alert.textContent = "Please fill out all the fields!"
+function getInfo(element) {
+  let name = element.parentElement.children[0];
 
-    setTimeout(() => {meal_alert.style.display = "none";}, 2500);
+  let info = JSON.parse(localStorage.getItem(name.textContent));
+
+  document.getElementById("meal-info-label").textContent = name.textContent;
+  document.getElementById("info-cals").textContent = `${
+    info?.calories ?? "N/A"
+  }`;
+  document.getElementById("info-protein").textContent = `${
+    info?.protein ?? "N/A"
+  }`;
+  document.getElementById("info-fat").textContent = `${info?.fat ?? "N/A"}`;
+  document.getElementById("info-carbs").textContent = `${info?.carbs ?? "N/A"}`;
 }
 
-function getInfo(element){
-    let name = element.parentElement.children[0];
+function editInfo(element) {
+  document.getElementById("save-info-button").style.display = "block";
+  element.style.display = "none";
 
-    let info = JSON.parse(localStorage.getItem(name.textContent));
+  let calorie_info = document.getElementById("info-cals");
+  let protein_info = document.getElementById("info-protein");
+  let fat_info = document.getElementById("info-fat");
+  let carb_info = document.getElementById("info-carbs");
 
-    document.getElementById("meal-info-label").textContent = name.textContent;
-    document.getElementById("info-cals").textContent = `${info?.calories ?? "N/A"}`;
-    document.getElementById("info-protein").textContent = `${info?.protein ?? "N/A"}`;
-    document.getElementById("info-fat").textContent = `${info?.fat ?? "N/A"}`;
-    document.getElementById("info-carbs").textContent = `${info?.carbs ?? "N/A"}`;
+  let calorie_input = document.createElement("input");
+  calorie_input.setAttribute("type", "text");
+  calorie_input.setAttribute("id", "cal-in");
+  calorie_input.style.width = "50%";
+  calorie_input.value = calorie_info.textContent;
+
+  let protein_input = document.createElement("input");
+  protein_input.setAttribute("type", "text");
+  protein_input.setAttribute("id", "protein-in");
+  protein_input.style.width = "50%";
+  protein_input.value = protein_info.textContent;
+
+  let fat_input = document.createElement("input");
+  fat_input.setAttribute("type", "text");
+  fat_input.setAttribute("id", "fat-in");
+  fat_input.style.width = "50%";
+  fat_input.value = fat_info.textContent;
+
+  let carb_input = document.createElement("input");
+  carb_input.setAttribute("type", "text");
+  carb_input.setAttribute("id", "carb-in");
+  carb_input.style.width = "50%";
+  carb_input.value = carb_info.textContent;
+
+  let body = element.parentElement.parentElement.children[1];
+
+  body.replaceChild(calorie_input, calorie_info);
+  body.replaceChild(protein_input, protein_info);
+  body.replaceChild(fat_input, fat_info);
+  body.replaceChild(carb_input, carb_info);
 }
 
-function editInfo(element){
-    document.getElementById("save-info-button").style.display = "block";
-    element.style.display = "none";
+function cancelEdit(element) {
+  if (document.getElementById("save-info-button").style.display === "none")
+    return;
 
-    let calorie_info = document.getElementById("info-cals");
-    let protein_info = document.getElementById("info-protein");
-    let fat_info = document.getElementById("info-fat");
-    let carb_info = document.getElementById("info-carbs");
+  document.getElementById("edit-button").style.display = "block";
+  document.getElementById("save-info-button").style.display = "none";
 
-    let calorie_input = document.createElement("input");
-    calorie_input.setAttribute("type", "text");
-    calorie_input.setAttribute("id", "cal-in");
-    calorie_input.style.width = "50%";
-    calorie_input.value = calorie_info.textContent;
+  let name = document.getElementById("meal-info-label");
+  let info = JSON.parse(localStorage[name.textContent]);
 
-    let protein_input = document.createElement("input");
-    protein_input.setAttribute("type", "text");
-    protein_input.setAttribute("id", "protein-in");
-    protein_input.style.width = "50%";
-    protein_input.value = protein_info.textContent;
+  let calories = document.createElement("p");
+  calories.setAttribute("id", "info-cals");
+  calories.textContent = info.calories;
 
-    let fat_input = document.createElement("input");
-    fat_input.setAttribute("type", "text");
-    fat_input.setAttribute("id", "fat-in");
-    fat_input.style.width = "50%";
-    fat_input.value = fat_info.textContent;
+  let protein = document.createElement("p");
+  protein.setAttribute("id", "info-protein");
+  protein.textContent = info.protein;
 
-    let carb_input = document.createElement("input");
-    carb_input.setAttribute("type", "text");
-    carb_input.setAttribute("id", "carb-in");
-    carb_input.style.width = "50%";
-    carb_input.value = carb_info.textContent;
+  let fat = document.createElement("p");
+  fat.setAttribute("id", "info-fat");
+  fat.textContent = info.fat;
 
-    let body = element.parentElement.parentElement.children[1];
+  let carbs = document.createElement("p");
+  carbs.setAttribute("id", "info-carbs");
+  carbs.textContent = info.carbs;
 
-    body.replaceChild(calorie_input, calorie_info);
-    body.replaceChild(protein_input, protein_info);
-    body.replaceChild(fat_input, fat_info);
-    body.replaceChild(carb_input, carb_info);
+  let body = element.parentElement.parentElement.children[1];
+
+  let calorie_input = document.getElementById("cal-in");
+  let protein_input = document.getElementById("protein-in");
+  let fat_input = document.getElementById("fat-in");
+  let carb_input = document.getElementById("carb-in");
+
+  body.replaceChild(calories, calorie_input);
+  body.replaceChild(protein, protein_input);
+  body.replaceChild(fat, fat_input);
+  body.replaceChild(carbs, carb_input);
 }
 
-function cancelEdit(element){
-    if(document.getElementById("save-info-button").style.display === "none") return;
+function saveInfo(element) {
+  document.getElementById("edit-button").style.display = "block";
+  element.style.display = "none";
 
-    document.getElementById("edit-button").style.display = "block";
-    document.getElementById("save-info-button").style.display = "none";
+  let name = document.getElementById("meal-info-label");
+  let calorie_input = document.getElementById("cal-in");
+  let protein_input = document.getElementById("protein-in");
+  let fat_input = document.getElementById("fat-in");
+  let carb_input = document.getElementById("carb-in");
 
-    let name = document.getElementById("meal-info-label");
-    let info = JSON.parse(localStorage[name.textContent])
+  let old_item = JSON.parse(localStorage.getItem(name.textContent));
 
-    let calories = document.createElement("p");
-    calories.setAttribute("id", "info-cals");
-    calories.textContent = info.calories;
+  localStorage.setItem(
+    name.textContent,
+    JSON.stringify({
+      type: mode,
+      name: name.textContent,
+      description: old_item.description,
+      calories: calorie_input.value,
+      protein: protein_input.value,
+      fat: fat_input.value,
+      carbs: carb_input.value,
+    })
+  );
 
-    let protein = document.createElement("p");
-    protein.setAttribute("id", "info-protein");
-    protein.textContent = info.protein;
+  let calories = document.createElement("p");
+  calories.setAttribute("id", "info-cals");
+  calories.textContent = calorie_input.value;
 
-    let fat = document.createElement("p");
-    fat.setAttribute("id", "info-fat");
-    fat.textContent = info.fat;
+  let protein = document.createElement("p");
+  protein.setAttribute("id", "info-protein");
+  protein.textContent = protein_input.value;
 
-    let carbs = document.createElement("p");
-    carbs.setAttribute("id", "info-carbs");
-    carbs.textContent = info.carbs;
+  let fat = document.createElement("p");
+  fat.setAttribute("id", "info-fat");
+  fat.textContent = fat_input.value;
 
-    let body = element.parentElement.parentElement.children[1];
+  let carbs = document.createElement("p");
+  carbs.setAttribute("id", "info-carbs");
+  carbs.textContent = carb_input.value;
 
-    let calorie_input = document.getElementById("cal-in");
-    let protein_input = document.getElementById("protein-in");
-    let fat_input = document.getElementById("fat-in");
-    let carb_input = document.getElementById("carb-in");
+  let body = element.parentElement.parentElement.children[1];
 
-    body.replaceChild(calories, calorie_input);
-    body.replaceChild(protein, protein_input);
-    body.replaceChild(fat, fat_input);
-    body.replaceChild(carbs, carb_input);
-}
-
-function saveInfo(element){
-    document.getElementById("edit-button").style.display = "block";
-    element.style.display = "none";
-
-    let name = document.getElementById("meal-info-label");
-    let calorie_input = document.getElementById("cal-in");
-    let protein_input = document.getElementById("protein-in");
-    let fat_input = document.getElementById("fat-in");
-    let carb_input = document.getElementById("carb-in");
-
-    let old_item = JSON.parse(localStorage.getItem(name.textContent));
-
-    localStorage.setItem(name.textContent, JSON.stringify({
-            "type" : mode,
-            "name" : name.textContent,
-            "description" : old_item.description,
-            "calories" : calorie_input.value,
-            "protein" : protein_input.value,
-            "fat" : fat_input.value,
-            "carbs" : carb_input.value
-        }))
-
-    let calories = document.createElement("p");
-    calories.setAttribute("id", "info-cals");
-    calories.textContent = calorie_input.value;
-
-    let protein = document.createElement("p");
-    protein.setAttribute("id", "info-protein");
-    protein.textContent = protein_input.value;
-
-    let fat = document.createElement("p");
-    fat.setAttribute("id", "info-fat");
-    fat.textContent = fat_input.value;
-
-    let carbs = document.createElement("p");
-    carbs.setAttribute("id", "info-carbs");
-    carbs.textContent = carb_input.value;
-
-    let body = element.parentElement.parentElement.children[1];
-
-    body.replaceChild(calories, calorie_input);
-    body.replaceChild(protein, protein_input);
-    body.replaceChild(fat, fat_input);
-    body.replaceChild(carbs, carb_input);
+  body.replaceChild(calories, calorie_input);
+  body.replaceChild(protein, protein_input);
+  body.replaceChild(fat, fat_input);
+  body.replaceChild(carbs, carb_input);
 }
--- profile.js
+++ profile.js
@@ -1,62 +1,136 @@
-let input = document.getElementById("search-input");
-let meals = [];
-let selectedMeal = null;
+function loadPosts() {
+  let posts = document.getElementById("posts");
+  let num_posts = localStorage.getItem("num-posts");
+  if (num_posts == 0) {
+    posts.appendChild(
+      (document.createElement("p").textContent =
+        "Looks like you don't have any posts yet...")
+    );
+  } else {
+    for (let i = 1; i <= num_posts; i++) {
+      let post_info = JSON.parse(localStorage.getItem(i));
+      let post = createPostHtml(
+        post_info.desc,
+        post_info?.meal ?? "none",
+        false
+      );
 
-for(var i in localStorage){
-        item = localStorage[i];
-
-        try{
-            item = JSON.parse(item);
-        }
-        catch (error){
-            continue;
-        }
-
-        if(!item.hasOwnProperty("type")){
-            continue;
-        }
-
-        meals.push(item);
-}
-
-input.onkeyup = searchMeals;
-
-function searchMeals(){
-    let searchList = meals.filter((meal) => {return meal.name.toLowerCase().startsWith(input.value.toLowerCase())});
-    let container = document.getElementById("meal-search-container");
-    container.innerText = '';
-    for(var i in searchList){
-        let el = document.createElement("button");
-        el.textContent = searchList[i].name;
-        el.className = "btn btn-outline-info";
-        el.setAttribute("onclick", "selectMeal(this)");
-        container.appendChild(el);
+      posts.appendChild(post);
     }
+  }
 }
 
-function selectMeal(element){
-    let container = document.getElementById("meal-search-container");
-    for(var i in container.children){
-        container.children[i].className = "btn btn-outline-info";
+function createPostHtml(description, meal, new_post) {
+  let post = document.createElement("div");
+  post.className = "post";
+
+  let username = document.createElement("a");
+  username.textContent = localStorage.getItem("this_user");
+  username.setAttribute("href", "profile.html"); // this will need to be fixed later
+  post.appendChild(username);
+
+  let desc = document.createElement("p");
+  desc.textContent = description;
+  post.appendChild(desc);
+
+  if (meal != "none") {
+    let item = JSON.parse(localStorage.getItem(meal));
+    createCard(item, post);
+  }
+
+  if (new_post) {
+    let num_posts = Number(localStorage.getItem("num-posts") ?? 0);
+    num_posts += 1;
+    localStorage.setItem("num-posts", num_posts);
+
+    localStorage.setItem(
+      num_posts,
+      JSON.stringify({
+        user: username.textContent,
+        desc: desc.textContent,
+        meal: meal ?? "none",
+      })
+    );
+  }
+
+  return post;
+}
+
+function post() {
+  let post_desc = document.getElementById("post-desc");
+
+  if (post_desc.value === "") {
+    let alert = document.getElementById("post-alert");
+    alert.style.display = "block";
+    alert.textContent = "Unable to post. Please provide a description!";
+
+    setTimeout(() => {
+      alert.style.display = "none";
+    }, 2500);
+  } else {
+    if (selectedMeal != null) {
+      var meal_info = JSON.parse(
+        localStorage.getItem(selectedMeal.textContent)
+      );
     }
-    element.className = "btn btn-info";
-    selectedMeal = element;
-    input.value = element.textContent;
+    let post = createPostHtml(post_desc.value, meal_info?.name ?? "none", true);
+    let posts = document.getElementById("posts");
+    posts.appendChild(post);
+
+    clearPostInput();
+  }
 }
 
-/*
-<div class="post">
-              Spencer
-              <img width="30px" src="Portrait.png" />
-              <p>Check out this meal! 1500 calories!</p>
-              <img src="breakfast.jpg" />
-              <div>
-                <input type="text" placeholder="comment" />
-                <button class="btn btn-info">></button>
-              </div>
-            </div>
-*/
+function createCard(item, container) {
+  let card = document.createElement("div");
+  card.className = "card";
 
-function post(){
-    
+  let html_img = document.createElement("img");
+
+  let body = document.createElement("div");
+  body.className = "card-body";
+
+  let meal_name = document.createElement("h5");
+  meal_name.textContent = item.name;
+
+  let meal_desc = document.createElement("p");
+  meal_desc.className = "card-text";
+  meal_desc.textContent = item.description;
+
+  let button = document.createElement("button");
+  button.className = "btn btn-info";
+  button.setAttribute("data-bs-target", "#meal-info");
+  button.setAttribute("data-bs-toggle", "modal");
+  button.setAttribute("onclick", "getInfo(this)");
+  button.textContent = "Info";
+
+  card.appendChild(html_img);
+  card.appendChild(body);
+  body.appendChild(meal_name);
+  body.appendChild(meal_desc);
+  body.appendChild(button);
+
+  container.appendChild(card);
+}
+
+function getInfo(element) {
+  let name = element.parentElement.children[0];
+
+  let info = JSON.parse(localStorage.getItem(name.textContent));
+
+  document.getElementById("meal-info-label").textContent = name.textContent;
+  document.getElementById("info-cals").textContent = `${
+    info?.calories ?? "N/A"
+  }`;
+  document.getElementById("info-protein").textContent = `${
+    info?.protein ?? "N/A"
+  }`;
+  document.getElementById("info-fat").textContent = `${info?.fat ?? "N/A"}`;
+  document.getElementById("info-carbs").textContent = `${info?.carbs ?? "N/A"}`;
+}
+
+function clearPostInput() {
+  document.getElementById("post-desc").value = "";
+  input.value = "";
+  document.getElementById("meal-search-container").textContent = "";
 }
