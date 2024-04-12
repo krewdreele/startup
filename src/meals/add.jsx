9ecbb9e3cdc095ce82182375f104d3894b532427
krewdreele <keele.drew00@gmail.com>
2024-04-12 13:29:32 -0600

    finished meal component

---
+++ src/meals/add.jsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export function Add({show, handleClose, type}) {
+    const [name, setName] = React.useState("");
+    const [desc, setDesc] = React.useState("");
+    const [cals, setCals] = React.useState(0);
+    const [protein, setProtein] = React.useState(0);
+    const [fat, setFat] = React.useState(0);
+    const [carbs, setCarbs] = React.useState(0);
+
+    const [alert, setAlert] = React.useState("");
+    const [search, setSearch] = React.useState("");
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add a Meal</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div id="add-meal-container">
+                    <div>
+                        Name:
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/> 
+                        Description:
+                        <textarea
+                        type="text"
+                        value={desc}
+                        onChange={(e) => setDesc(e.target.value)}
+                        ></textarea>
+                        Calories:
+                        <input type="text" value={cals} onChange={(e) => setCals(e.target.value)}/> 
+                        Protein:
+                        <input
+                        type="text"
+                        value={protein}
+                        onChange={(e) => setProtein(e.target.value)}
+                        />
+                        Fat:
+                        <input type="text" value={fat} onChange={(e) => setFat(e.target.value)}/> 
+                        Carbs:
+                        <input
+                        type="text"
+                        value={carbs}
+                        onChange={(e) => setCarbs(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>Search for ingredient:</label>
+                        <input
+                        type="text"
+                        placeholder="ex: 1oz rice"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        />
+                        <Button
+                        variant='outline-info'
+                        onClick={() => getThirdPartyMeal(setAlert, search, desc, setDesc, cals, setCals, protein, setProtein, fat, setFat, carbs, setCarbs)}
+                        >
+                        &#128269;
+                        </Button>
+
+                        {alert && 
+                        <div
+                        className="alert alert-danger"
+                        >{alert}</div>}
+                    </div>
+            </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant='outline-secondary'
+                        onClick={() => handleClose}
+                >
+                Cancel
+                </Button>
+                <Button
+                onClick={() => {handleClose();
+                saveMeal(name, desc, cals, protein, fat, carbs, type);}}  
+                >
+                Save
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+async function saveMeal(name, desc, cals, protein, fat, carbs, type){
+    let request = {
+      username: localStorage.getItem("this-user"),
+      type: type,
+      name: name,
+      description: desc,
+      calories: cals,
+      protein: protein,
+      fat: fat,
+      carbs: carbs,
+    };
+
+    const response = await fetch(`api/meal`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(request),
+    });
+}
+
+async function getThirdPartyMeal(setAlert, search, desc, setDesc, cals, setCals, protein, setProtein, fat, setFat, carbs, setCarbs) {
+  /*
+  ID - 37ab6c8a
+  key - 6bd1526b7c00fd3d34cdf6b579bc8a89	
+  */
+
+  setAlert("");
+
+  const response = await fetch(
+    `https://api.edamam.com/api/nutrition-data?app_id=37ab6c8a&app_key=6bd1526b7c00fd3d34cdf6b579bc8a89&ingr=${search}`
+  );
+
+  try {
+    let food = await response.json();
+
+    if (
+      food &&
+      food.ingredients &&
+      food.calories &&
+      food.totalNutrients["PROCNT"].quantity &&
+      food.totalNutrients["FAT"].quantity &&
+      food.totalNutrients["CHOCDF.net"].quantity
+    ) {
+      for (let i in food.ingredients) {
+        setDesc(desc.concat(
+          `${food.ingredients[i].text}\n`
+        ));
+      }
+
+      setCals(cals + food.calories);
+
+      setProtein(
+        protein +
+        Math.round(food.totalNutrients["PROCNT"].quantity));
+
+      setFat(
+        fat + Math.round(food.totalNutrients["FAT"].quantity));
+
+      setCarbs(
+        carbs + 
+        Math.round(food.totalNutrients["CHOCDF.net"].quantity));
+    } else {
+      throw new Error("Couldn't find food");
+    }
+  } catch (error) {
+    setAlert(error.message);
+  }
+}
--- src/meals/meals.jsx
+++ src/meals/meals.jsx
@@ -1,60 +1,85 @@
 import React, { useState, useEffect } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
 import { MealCard } from './card';
-import "./meals.css"
-
-let breakfast = [];
-let lunch = [];
-let dinner = [];
+import { Add } from './add';
+import "./meals.css";
 
 export function Meals() {
   const [index, setIndex] = useState(0);
+  const [breakfast, setBreakfast] = useState([]);
+  let [lunch, setLunch] = useState([]);
+  let [dinner, setDinner] = useState([]);
+
+  const [mode, setMode] = React.useState("Breakfast");
+  const [showAdd, setShowAdd] = React.useState(false);
 
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
+  }
+
+  const handleClose = () => {
+    setShowAdd(false);
   };
 
   useEffect(() => {
-    loadMeals(breakfast, lunch, dinner);
-  }, []);
+    let user = localStorage.getItem("this-user");
+
+    async function loadMeals(){
+      let response = await fetch(`api/meals?user=${user}`);
+
+      if(response.ok){
+        let meals = await response.json();
+
+        setBreakfast(meals.filter((meal) => meal.type == "Breakfast"));
+        setLunch(meals.filter((meal) => meal.type == "Lunch"));
+        setDinner(meals.filter((meal) => meal.type == "Dinner"));
+      }
+    } loadMeals();
+  }, [!showAdd]);
 
   return (
-    <Carousel id="carousel" activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item className='container'>
+    <main id="meals">
+      <Add show={showAdd} handleClose={handleClose} type={mode}></Add>
+      <Tabs
+        activeKey={mode}
+        onSelect={(k) => setMode(k)}
+        className="mb-3"
+      >
+      <Tab eventKey="Breakfast" title="Breakfast">
         <h2>Breakfast</h2>
-        <div className="card-container">{breakfast}</div>
-      </Carousel.Item>
-      <Carousel.Item className='container'>
+        <div className="card-container">{
+          breakfast.map(function(meal) {
+            return <MealCard key={meal.name} item={meal}></MealCard>;
+          })
+          }
+        </div>
+        <Button onClick={() => {setShowAdd(true); setMode("Breakfast");}}> + </Button>
+      </Tab>
+      <Tab eventKey="Lunch" title="Lunch">
         <h2>Lunch</h2>
-        <div className="card-container">{lunch}</div>
-      </Carousel.Item>
-      <Carousel.Item className='container'>
+        <div className="card-container">{
+          lunch.map(function(meal){
+            return <MealCard key={meal.name} item={meal}></MealCard>;
+          })
+          }
+        </div>
+        <Button onClick={() => {setShowAdd(true); setMode("Lunch");}}> + </Button>
+      </Tab>
+      <Tab eventKey="Dinner" title="Dinner">
         <h2>Dinner</h2>
-        <div className="card-container">{dinner}</div>
-      </Carousel.Item>
-    </Carousel>
+        <div className="card-container">{
+          dinner.map(function(meal){
+            return <MealCard key={meal.name} item={meal}></MealCard>;
+          })
+          }
+        </div>
+        <Button onClick={() => {setShowAdd(true); setMode("Dinner");}}> + </Button>
+      </Tab>
+    </Tabs>  
+    </main>
   );
 }
 
 
-async function loadMeals(breakfast, lunch, dinner) {
-
-  let user = localStorage.getItem("this-user");
-
-  let response = await fetch(`api/meals?user=${user}`);
-
-  if(response.ok){
-    let meals = await response.json();
-
-    for (let i in meals) {
-      let item = meals[i];
-      if (item.type == "Breakfast") {
-        breakfast.push(<MealCard key={i} item={item}></MealCard>)
-      } else if (item.type == "Lunch") {
-        lunch.push(<MealCard key={i} item={item} ></MealCard>)
-      } else if (item.type == "Dinner") {
-        dinner.push(<MealCard key={i} item={item}></MealCard>)
-      }
-    } 
-  }
-}
